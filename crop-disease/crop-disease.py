6aadd620625e1d7d0449a4a7cc7edbccc6772b99
Simon Hüning <huening.simon@gmail.com>
2018-10-09 00:56:01 +0200

    transform gray to rgb

--- crop-disease/crop-disease.py
+++ crop-disease/crop-disease.py
@@ -36,6 +36,7 @@ import geojson
 from geojson.feature import Feature
 import skimage.draw
 import skimage
+from skimage.external import tifffile
 import skimage.color
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
@@ -44,7 +45,8 @@ ROOT_DIR = os.path.abspath("../")
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
-
+from mrcnn import visualize
+from IPython.display import display, Image
 # Path to trained weights file
 COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
@@ -61,13 +63,11 @@ class CropDiseaseConfig(Config):
     """Configuration for training on the crop disease dataset.
     Derives from the base Config class and overrides some values.
     """
-    BACKBONE = "resnet50"
+    # BACKBONE = "resnet50"
     
     # Give the configuration a recognizable name
     NAME = "crop-disease"
 
-    # We use a GPU with 12GB memory, which can fit two images.
-    # Adjust down if you use a smaller GPU.
     IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
@@ -75,60 +75,59 @@ class CropDiseaseConfig(Config):
     NUM_CLASSES = 1 + 1  
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 1
-    
-    # test
-    IMAGE_RESIZE_MODE = "none"
+    STEPS_PER_EPOCH = 100
     
     # only one value because we use grayscale images
     # see https://github.com/matterport/Mask_RCNN/wiki#training-with-rgb-d-or-grayscale-images
-    MEAN_PIXEL = np.array([0])
+    # MEAN_PIXEL = np.array([0.5])
 
+    # IMAGE_CHANNEL_COUNT = 1
+
+    IMAGE_RESIZE_MODE = "square"
+    IMAGE_MIN_DIM = 128
+    IMAGE_MAX_DIM = 128
     
-
+    USE_MINI_MASK = True
+    
+    DETECTION_MIN_CONFIDENCE = 0.6
+    
+    MEAN_PIXEL = np.array([123.7, 123.7, 123.7])
 
 ############################################################
 #  Dataset
 ############################################################
 
+
 class CropDiseaseDataset(utils.Dataset):
 
     def load_crop_disease(self, dataset_dir, subset):
         """Load feature subset of the crop disease dataset.
-        dataset_dir: Root directory of the dataset.
+        subset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
         """
+        
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        subset_dir = os.path.join(dataset_dir, subset)
+        
         # Add classes. We have only one class to add.
         classes = json.load(open(os.path.join(dataset_dir, "diseases.json")))
         for key, value in classes.items():
             self.add_class("crop-disease", key, value)
         
-        # Train or validation dataset?
-        assert subset in ["train", "val"]
-        dataset_dir = os.path.join(dataset_dir, subset)
-
         # Load annotations
-        annotations = geojson.load(open(os.path.join(dataset_dir, "regions.json")))
+        annotations = geojson.load(open(os.path.join(subset_dir, "regions.geojson")))
 
         # Add images
         for feature in annotations.features:
-            # Get the x, y coordinaets of points of the polygons that make up
-            # the outline of each object instance. 
-            polygons = feature.geometry.coordinates
-
             # load_mask() needs the image size to convert polygons to masks.
-            for product in feature.properties["sentinelproducts"]:
-                image_path = os.path.join(dataset_dir, feature.properties["id"] + "_" + product + ".tif")
-                image = skimage.io.imread(image_path, plugin="pil")
-                height, width = image.shape[:2]
-    
-                self.add_image(
-                    "crop-disease",
-                    image_id=feature.properties["id"] + "_" + product,
-                    path=image_path,
-                    width=width, height=height,
-                    polygons=polygons,
-                    annotations=feature.properties["disease"])
+            image_path = os.path.join(subset_dir, feature.properties["id"] + ".tif")
+
+            self.add_image(
+                "crop-disease",
+                image_id=feature.properties["id"],
+                path=image_path,
+                annotation=feature.properties["disease"])
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -143,11 +142,15 @@ class CropDiseaseDataset(utils.Dataset):
             return super(self.__class__, self).load_mask(image_id)
 
         # read mask that was saved during generating the ndvi
-        mask = np.load(image_info["path"].replace("tif", "mask")).mask != True
+        masked_arr = np.load(image_info["path"].replace("tif", "mask"))
+        # the mask needs to be inverted and reshaped since it comes in a shape like [1, height, width]
+        # but we need [height, width, 1] (the one represents the number of instances, 
+        # but since we only have one instance of an image we use 1)
+        mask = masked_arr.mask != True
+        mask = np.expand_dims(mask[0], axis=2)
         
-        print(mask.shape[-1])
         # Return mask, and array of class IDs of each instance. 
-        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32) # np.array(image_info["annotations"])
+        return mask.astype(np.bool), np.array([image_info["annotation"]]).astype(np.int32)  # np.array(image_info["annotations"])
 
     def image_reference(self, image_id):
         """Return the path of the image."""
@@ -158,34 +161,126 @@ class CropDiseaseDataset(utils.Dataset):
             super(self.__class__, self).image_reference(image_id)
             
     def load_image(self, image_id):
-        """Override to return grayscale image"""
+        """Override to return image"""
         # Load image
-        image = skimage.io.imread(self.image_info[image_id]['path'])
-        return image
+        image = skimage.io.imread(self.image_info[image_id]['path']) 
+        rgb_image = skimage.color.gray2rgb(image * 255)
+        return rgb_image 
 
 
 def train(model):
     """Train the model."""
     # Training dataset.
+    print("Loading training dataset")
     dataset_train = CropDiseaseDataset()
     dataset_train.load_crop_disease(args.dataset, "train")
     dataset_train.prepare()
+    
+    print("Image Count: {}".format(len(dataset_train.image_ids)))
+    print("Class Count: {}".format(dataset_train.num_classes))
+    for i, info in enumerate(dataset_train.class_info):
+        print("{:3}. {:50}".format(i, info['name']))
+        
+    image_ids = np.random.choice(dataset_train.image_ids, 4)
+    for image_id in image_ids:
+        image = dataset_train.load_image(image_id)
+        mask, class_ids = dataset_train.load_mask(image_id)
+        visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
 
     # Validation dataset
+    print("Loading validation set")
     dataset_val = CropDiseaseDataset()
     dataset_val.load_crop_disease(args.dataset, "val")
     dataset_val.prepare()
 
-    print("Train everything")
+    print("Image Count: {}".format(len(dataset_val.image_ids)))
+    print("Class Count: {}".format(dataset_val.num_classes))
+    for i, info in enumerate(dataset_val.class_info):
+        print("{:3}. {:50}".format(i, info['name']))
+
+    print("Start training")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=1,
                 layers='all')
+    
+    
+    
+    
+def color_splash(image, mask):
+    """Apply color splash effect.
+    image: RGB image [height, width, 3]
+    mask: instance segmentation mask [height, width, instance count]
+    Returns result image.
+    """
+    # Make a grayscale copy of the image. The grayscale copy still
+    # has 3 RGB channels, though.
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    # Copy color pixels from the original color image where mask is set
+    if mask.shape[-1] > 0:
+        # We're treating all instances as one, so collapse the mask into one layer
+        mask = (np.sum(mask, -1, keepdims=True) >= 1)
+        splash = np.where(mask, image, gray).astype(np.uint8)
+    else:
+        splash = gray.astype(np.uint8)
+    return splash
 
+
+def detect_and_color_splash(model, image_path=None, video_path=None):
+    assert image_path or video_path
+
+    # Image or video?
+    if image_path:
+        # Run model detection and generate the color splash effect
+        print("Running on {}".format(image_path))
+        # Read image
+        image = skimage.color.gray2rgb(skimage.io.imread(image_path))
+        # Detect objects
+        r = model.detect([image], verbose=1)[0]
+        # Color splash
+        splash = color_splash(image, r['masks'])
+        # Save output
+        file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
+        skimage.io.imsave(file_name, splash)
+    elif video_path:
+        import cv2
+        # Video capture
+        vcapture = cv2.VideoCapture(video_path)
+        width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = vcapture.get(cv2.CAP_PROP_FPS)
+
+        # Define codec and create video writer
+        file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
+        vwriter = cv2.VideoWriter(file_name,
+                                  cv2.VideoWriter_fourcc(*'MJPG'),
+                                  fps, (width, height))
+
+        count = 0
+        success = True
+        while success:
+            print("frame: ", count)
+            # Read next image
+            success, image = vcapture.read()
+            if success:
+                # OpenCV returns images as BGR, convert to RGB
+                image = image[..., ::-1]
+                # Detect objects
+                r = model.detect([image], verbose=0)[0]
+                # Color splash
+                splash = color_splash(image, r['masks'])
+                # RGB -> BGR to save image to video
+                splash = splash[..., ::-1]
+                # Add image to video writer
+                vwriter.write(splash)
+                count += 1
+        vwriter.release()
+        print("Saved to ", file_name)
 
 ############################################################
 #  Training
 ############################################################
+
 
 if __name__ == '__main__':
     import argparse
@@ -214,9 +309,6 @@ if __name__ == '__main__':
     # Validate arguments
     if args.command == "train":
         assert args.dataset, "Argument --dataset is required for training"
-    elif args.command == "splash":
-        assert args.image or args.video,\
-               "Provide --image or --video to apply color splash"
 
     print("Weights: ", args.weights)
     print("Dataset: ", args.dataset)
@@ -226,11 +318,16 @@ if __name__ == '__main__':
     if args.command == "train":
         config = CropDiseaseConfig()
     else:
+
         class InferenceConfig(CropDiseaseConfig):
             # Set batch size to 1 since we'll be running inference on
             # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
             GPU_COUNT = 1
             IMAGES_PER_GPU = 1
+            #IMAGE_RESIZE_MODE = "square"
+            #IMAGE_MIN_DIM = 2048
+            
+
         config = InferenceConfig()
     config.display()
 
@@ -262,8 +359,8 @@ if __name__ == '__main__':
     if args.weights.lower() == "coco":
         # Exclude the last layers because they require a matching
         # number of classes
-        # need to ignore first layer for grayscale images ub case of pre-trained weights
-        model.load_weights(weights_path, by_name=True, exclude=["conv1",
+        # need to ignore first layer for grayscale images in case of pre-trained weights
+        model.load_weights(weights_path, by_name=True, exclude=[
             "mrcnn_class_logits", "mrcnn_bbox_fc",
             "mrcnn_bbox", "mrcnn_mask"])
     else:
@@ -274,15 +371,17 @@ if __name__ == '__main__':
         train(model)
     elif args.command == "detect":
         # load image
-        image = skimage.io.imread("../sentineldata/products/ndvi/ef21dc70-7a1e-4bb5-99d0-6b7778e71377_S2A_MSIL2A_20180708T101031_N0208_R022_T32TPQ_20180708T133033.SAFE.tif")
-        
+        # detect_and_color_splash(model, image_path="ndvi.tif")
         # Run detection
+         
+        image = skimage.color.gray2rgb(skimage.io.imread("ndvi.tif") * 255)
         results = model.detect([image], verbose=1)
         
         # Visualize results
         r = results[0]
         visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
-                                    class_names, r['scores'])
+                                       ["BG", "Anthracnose"], r['scores'])
+        
     else:
         print("'{}' is not recognized. "
               "Use 'train' or 'detect'".format(args.command))
